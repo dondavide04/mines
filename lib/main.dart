f6840e50ff4978f55a9cc08b7025704be4811278
Davide Dongiovanni <>
2021-12-15 19:19:34 +0100

    add simple home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mines/home.dart';
 
 void main() {
   runApp(const App());
@@ -7,15 +8,12 @@ void main() {
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mines',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const Text('demo'),
+      initialRoute: '/',
+      routes: {'/': (_) => const Home()},
     );
   }
 }
