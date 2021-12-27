12cbd22afc81795d9d561224bf064d8eb23bef57
Davide Dongiovanni <>
2021-12-27 18:08:04 +0100

    add base clickable grid

--- lib/routes/game/box.dart
+++ lib/routes/game/box.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-class Box extends StatelessWidget {
-  const Box({Key? key}) : super(key: key);
+class Box extends StatefulWidget {
+  final int content;
+  final double size = 32;
+
+  const Box({Key? key, required this.content}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _BoxState();
+}
+
+class _BoxState extends State<Box> {
+  bool _isVisible = false;
+
+  _handleTap() {
+    if (!_isVisible) {
+      setState(() {
+        _isVisible = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('content'),
-      padding: const EdgeInsets.all(8),
+    return InkWell(
+      child: SizedBox(
+        child: _isVisible
+            ? Center(
+                child: Text(widget.content.toString()),
+              )
+            : Container(
+                // decoration: BoxDecoration(
+                //   borderRadius: BorderRadius.circular(20),
+                // ),
+                color: Theme.of(context).primaryColorLight,
+              ),
+        width: widget.size,
+        height: widget.size,
+      ),
+      onTap: _isVisible ? null : _handleTap,
     );
   }
 }
--- lib/routes/game/game.dart
+++ lib/routes/game/game.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:mines/routes/game/box.dart';
 
-class Game extends StatelessWidget {
+const gameSize = 9;
+const totalMines = 27;
+
+List<List<int>> _mkGameBoard() {
+  final flatBoard = List.filled(totalMines, 1, growable: true);
+  flatBoard.addAll(List.filled((gameSize * gameSize) - totalMines, 0));
+  flatBoard.shuffle();
+  return List.generate(gameSize, (index) {
+    final start = index * gameSize;
+    final end = start + gameSize;
+    return flatBoard.sublist(start, end);
+  }, growable: false);
+}
+
+class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  final List<List<int>> board;
+
+  _GameState() : board = _mkGameBoard();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        widthFactor: double.infinity,
-        heightFactor: double.infinity,
-        child: SafeArea(
-          minimum: const EdgeInsets.all(8),
-          child: Column(
-            children: [
-              const Box(),
-              ElevatedButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Text('back'),
-              ),
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        alignment: Alignment.center,
+        child: Wrap(
+          direction: Axis.vertical,
+          spacing: 8,
+          children: board
+              .map(
+                (row) => Wrap(
+                  spacing: 8,
+                  children: row.map((e) => Box(content: e)).toList(),
+                ),
+              )
+              .toList(),
         ),
       ),
     );
