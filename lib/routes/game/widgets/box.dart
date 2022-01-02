ed4d3d3aa4772c8f9f68f4abd33454799a8493cb
Davide Dongiovanni <>
2022-01-02 18:43:58 +0100

    add flag

--- lib/routes/game/widgets/box.dart
+++ lib/routes/game/widgets/box.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mines/routes/game/widgets/cell.dart';
 
-class Box extends StatelessWidget {
+class Box extends StatefulWidget {
   final CellWidget content;
   final int xCoord;
   final int yCoord;
@@ -19,24 +19,40 @@ class Box extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<Box> createState() => _BoxState();
+}
+
+class _BoxState extends State<Box> {
+  bool _isFlagged = false;
+
+  void _toggleFlag() => setState(() {
+        _isFlagged = !_isFlagged;
+      });
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
       child: SizedBox(
-        child: isVisible
+        child: widget.isVisible
             ? Container(
                 padding: const EdgeInsets.all(8),
-                child: content,
+                child: widget.content,
               )
             : Container(
                 // decoration: BoxDecoration(
                 //   borderRadius: BorderRadius.circular(20),
                 // ),
                 color: Theme.of(context).primaryColorLight,
+                child:
+                    _isFlagged ? Image.asset('assets/images/flag.png') : null,
               ),
-        width: size,
-        height: size,
+        width: widget.size,
+        height: widget.size,
       ),
-      onTap: () => onTap(xCoord, yCoord),
+      onTap: () => _isFlagged
+          ? _toggleFlag()
+          : widget.onTap(widget.xCoord, widget.yCoord),
+      onLongPress: _toggleFlag,
     );
   }
 }
