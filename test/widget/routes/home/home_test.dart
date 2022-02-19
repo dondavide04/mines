f884c77e7efd4fc279d1162d3e180d42c728d130
Davide Dongiovanni <dongiovannidavide@gmail.com>
2022-02-19 12:20:32 +0100

    wip test file

---
+++ test/widget/routes/home/home_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mines/routes/home/home.dart';
+import 'package:mines/routes/home/widgets/settingsDialog.dart';
+
+void main() {
+  MaterialApp _app() => const MaterialApp(home: Home());
+  testWidgets('Renders without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(_app());
+    final startBtn = find.text('Start');
+    expect(startBtn, findsOneWidget);
+  });
+
+  testWidgets('Pressing start button opens the settings modal',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(_app());
+    final startBtn = find.text('Start');
+    await tester.tap(startBtn);
+    // TODO this doesn't work
+    // expect(find.byType(SettingsDialog), findsOneWidget);
+  });
+}
